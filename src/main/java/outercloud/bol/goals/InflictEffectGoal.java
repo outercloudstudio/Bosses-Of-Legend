4a23ba102e656c6dd814ca8f31fac9c2337e01aa
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-02-01 02:32:47 -0600

    Inflict Effect Testing

--- src/main/java/outercloud/bol/goals/InflictEffectGoal.java
+++ src/main/java/outercloud/bol/goals/InflictEffectGoal.java
@@ -2,27 +2,41 @@ package outercloud.bol.goals;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import outercloud.bol.BossesOfLegend;
 
 import java.util.EnumSet;
 
 public class InflictEffectGoal extends Goal {
     private final MobEntity mob;
-
+    
     public InflictEffectGoal(MobEntity mob) {
         this.mob = mob;
-        this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
+        setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
     }
 
     @Override
     public boolean canStart() {
-        LivingEntity livingEntity = this.mob.getTarget();
-        return livingEntity != null && livingEntity.isAlive() && this.mob.canTarget(livingEntity);
+        if (mob.getRandom().nextInt(toGoalTicks(100)) != 0) return false;
+
+        LivingEntity livingEntity = mob.getTarget();
+        return livingEntity != null && livingEntity.isAlive() && mob.canTarget(livingEntity);
     }
 
     @Override
     public void start() {
-        BossesOfLegend.LOGGER.info("Inflict Effect");
+        mob.getTarget().addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 5, 4), mob);
+
+        ServerWorld world = (ServerWorld) mob.getWorld();
+        world.spawnParticles(ParticleTypes.SONIC_BOOM, mob.getX(), mob.getY() + 3, mob.getZ(), 1, 0, 0, 0, 0);
+    }
+
+    @Override
+    public boolean shouldContinue() {
+        return false;
     }
 }
--- src/main/java/outercloud/bol/mixin/MobEntityMixin.java
+++ src/main/java/outercloud/bol/mixin/MobEntityMixin.java
@@ -9,20 +9,21 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import outercloud.bol.BossesOfLegend;
 import outercloud.bol.goals.InflictEffectGoal;
 
 @Mixin(MobEntity.class)
-public class MobEntityMixin {
+public abstract class MobEntityMixin {
 	@Accessor
-	private GoalSelector getGoalSelector() {
-		throw new NotImplementedException();
-	}
+	abstract GoalSelector getGoalSelector();
 
-	@Inject(at = @At("HEAD"), method = "initGoals")
-	private void initGoals(CallbackInfo info) {
+	@Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
+	private void init(CallbackInfo info) {
 		MobEntity me = (MobEntity) (Object) this;
 
+		if (me.getWorld() == null || me.getWorld().isClient) return;
+
 		GoalSelector goalSelector = getGoalSelector();
-		goalSelector.add(0, new InflictEffectGoal(me));
+		goalSelector.add(1, new InflictEffectGoal(me));
 	}
 }
